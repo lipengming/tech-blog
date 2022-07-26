fc40db84957c7ae772b0c1d42b50b0f92b0f2446
cubber.li <cubber.zh@gmail.com>
2022-07-26 20:14:41 +0800

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 var config = require('../config');
 
-mongoose.connect(config.db, function (err) {
+mongoose.connect(config.db,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+ function (err) {
   if (err) {
     console.error('connect to %s error: ', config.db, err.message);
     process.exit(1);
